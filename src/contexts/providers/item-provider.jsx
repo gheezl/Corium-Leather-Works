01941556025a5bd7ffaa326becff59c5b5a8004c
gheezl <gheezl25@gmail.com>
2020-08-24 11:56:07 -0400

    Added the images to the project

--- src/contexts/providers/item-provider.jsx
+++ src/contexts/providers/item-provider.jsx
@@ -1,6 +1,15 @@
-import React, { createContext, useState, useEffect, Component } from "react"
+import React, { createContext, Component } from "react"
 
-const ItemsContext = createContext()
+import multiPurposeBeltLoop1 from "../../images/multipurpose-belt-loop-1.jpg"
+import multiPurposeBeltLoop2 from "../../images/multipurpose-belt-loop-2.jpg"
+import multiPurposeBeltLoop3 from "../../images/multipurpose-belt-loop-3.jpg"
+
+import braidedBracelet1 from "../../images/braided-bracelet-1.jpg"
+import braidedBracelet2 from "../../images/braided-bracelet-2.jpg"
+import braidedBracelet3 from "../../images/braided-bracelet-3.jpg"
+import braidedBracelet4 from "../../images/braided-bracelet-4.jpg"
+
+export const ItemsContext = createContext()
 
 
 class ItemsProvider extends Component {
@@ -12,14 +21,14 @@ class ItemsProvider extends Component {
                 {
                     name: "Multipurpose Belt Loop",
                     price: "$15",
-                    despricption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-                    img: "%PUBLIC_URL%/multipurpos-belt-loop-1.jpg"
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+                    img: [multiPurposeBeltLoop1, multiPurposeBeltLoop2, multiPurposeBeltLoop3]
                 },
                 {
                     name: "Braided Braclet",
                     price: "%15",
-                    despricption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-                    img: "%PUBLIC_URL%/braided-bracelt-1.jpg"
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+                    img: [braidedBracelet1, braidedBracelet2, braidedBracelet3, braidedBracelet4]
                 }
             ]
         }
