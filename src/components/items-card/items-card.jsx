43d0ba872729043832f764eb87db0ee7c7738bc5
gheezl <gheezl25@gmail.com>
2020-08-26 10:58:42 -0400

    Readded the next and previous image buttons

--- src/components/items-card/items-card.jsx
+++ src/components/items-card/items-card.jsx
@@ -6,27 +6,26 @@ import './items-card.css';
 const ItemsCard = (item) => {
     const [toggle, setToggle] = useState(false)
     const [toggleImage, setToggleImage] = useState(false)
-    // const [increment, setIncrement] = useState(0)
+    const [increment, setIncrement] = useState(0)
 
 
-    // const onRight = () => {
-    //     if (increment < 2) {
-    //         setIncrement(increment + 1)
-    //     }
-    // }
+    const onRight = () => {
+        if (increment < 2) {
+            setIncrement(increment + 1)
+        }
+    }
 
-    // const onLeft = () => {
-    //     if (increment > 0) {
-    //         setIncrement(increment - 1)
-    //     }
-    // }
+    const onLeft = () => {
+        if (increment > 0) {
+            setIncrement(increment - 1)
+        }
+    }
 
-    const increment = 0
 
     return (
         <Fragment>
             <div onMouseEnter={() => setToggle(true)} onMouseLeave={() => setToggle(false)} className="item-border">
-                {/* <button onClick={onLeft} className="left">L</button> */}
+
                 <img
                     className="item-image"
                     alt=""
@@ -34,14 +33,16 @@ const ItemsCard = (item) => {
                     onClick={() => setToggleImage(true)}
                     width="384" height="512"
                 />
-                {/* <button onClick={onRight} className="right">R</button> */}
+
                 <a href={item.item.url} className="buy-product" >Buy Product on Amazon</a>
                 {toggle ? (<span className="item-name">{item.item.name} : {item.item.price}</span>) : (null)}
                 {toggleImage
                     ? (<Fragment>
                         <div className="large-image-border">
                             <button onClick={() => setToggleImage(false)} className="x-button">X</button>
+                            <button onClick={onLeft} className="left">Previous Image</button>
                             <img className="large-image" alt="" src={item.item.img[increment]} />
+                            <button onClick={onRight} className="right">Next Image</button>
                         </div>
                     </Fragment>)
                     : (null)
